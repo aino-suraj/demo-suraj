ec1629f3c884d04377b3076dba7e7e266f9b43ed
comp12 <comp12@comp12-All>
2018-07-25 17:04:10 +0530

    first commit

---
+++ src/test/java/ApplicationTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+	@Test
+	public void test() {
+		SampleClass obj=new SampleClass();
+			
+		assertEquals(obj.getSample(),"maven_sample");
+	
+	}
+
+}
